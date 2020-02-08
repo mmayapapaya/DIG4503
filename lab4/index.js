96b86e5e77f385e20d283ce33e45555cc8613c05
Maya Lindseth <maya@lindseth.org>
2020-02-07 23:01:51 -0500

    workin

--- lab4/index.js
+++ lab4/index.js
@@ -1,5 +1,6 @@
-const Express = require("Express");
+const Express = require('Express');
 const app = Express();
+const pokemons = require('json-pokemon');
 const port = 80;
 
 App.listen(80, function() {
@@ -8,4 +9,12 @@ App.listen(80, function() {
 
 // /id/:id returns JSON obeject matching pokemon ID orrr an error if ID doesn't exist
 
+App.get("/id/:id", function(req, res){
+
+});
+
 // /name/:name returns a json object matching pokemon name or an error if name doesn't exist
+
+App.get("/name/:name", function(req, res){
+
+});
