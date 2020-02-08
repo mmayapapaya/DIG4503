688621f8c9d7568a9487e550568df0a4bcb963f1
Maya Lindseth <maya@lindseth.org>
2020-02-07 23:21:18 -0500

    pokepokepokepoke

--- lab4/index.js
+++ lab4/index.js
@@ -1,11 +1,13 @@
-const Express = require('Express');
+const Express = require("Express");
 const app = Express();
-const pokemons = require('json-pokemon');
+const chalk = require("Chalk");
 const port = 80;
 
-App.listen(80, function() {
-	console.log("Server Running Babyyy!");
-});
+const pokemon = require('json-pokemon');
+const getPokemons = require('json-pokemon/getPokemon');
+const pokemons = getPokemons();
+const firstPokemon = getPokemons.getPokemonById(1);
+const poke = getPokemons.getPokemonByName('poke');
 
 // /id/:id returns JSON obeject matching pokemon ID orrr an error if ID doesn't exist
 
@@ -17,4 +19,10 @@ App.get("/id/:id", function(req, res){
 
 App.get("/name/:name", function(req, res){
 
+});
+
+
+
+App.listen(port, () => {
+	console.log("Server Running Babyyy!");
 });
